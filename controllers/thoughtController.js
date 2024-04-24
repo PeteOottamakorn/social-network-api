c4b64f883f0993ffb3c4cb8d82ed65ed1eadbd5d
ItsToastCrumb <160540794+ItsToastCrumb@users.noreply.github.com>
2024-04-24 00:34:48 -0600

    adding more boilerplate code, wrote some pseudocode

---
+++ controllers/thoughtController.js
@@ -0,0 +1,11 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  // Get all thoughts
+  // Get a single thought by its _id
+  // Create a new thought (need to push created thought's _id to the associated user's thoughts array)
+  // Update a thought by its _id
+  // Delete a thought by its _id
+  // Create a reaction stored in the associated thought's reactions array
+  // Delete a reaction by its reactionId value
+};
---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  // Get all users
+  // Get a single user by its _id populated with thought and friend data
+  // Post a new user
+  // Update a user by its _id
+  // Delete a user by its _id
+  // Add a new friend to a user's friend list
+  // Delete a friend from a user's friend list
+};
